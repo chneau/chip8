cb57f7adccd474d35403748390da86ca11252352
chneau <charles63500@gmail.com>
2018-10-11 22:21:29 +0100

    fixes.

--- pkg/chip8/cycle.go
+++ pkg/chip8/cycle.go
@@ -6,7 +6,6 @@ import (
 
 func (c *Chip8) emulateCycle() {
 	op := uint16(c.Mem[c.PC])<<8 | uint16(c.Mem[c.PC+1])
-
 	switch op & 0xf000 {
 	case 0x0000:
 		switch op & 0x0fff {
@@ -18,6 +17,8 @@ func (c *Chip8) emulateCycle() {
 			c.SP--
 			c.PC = c.Stack[c.SP]
 			c.PC += 2
+		default:
+			panic("Not implemented")
 		}
 	case 0x1000:
 		c.PC = op & 0x0fff
@@ -26,89 +27,110 @@ func (c *Chip8) emulateCycle() {
 		c.SP++
 		c.PC = op & 0x0fff
 	case 0x3000:
-		if uint16(c.V[op&0x0f00>>8]) == op&0x00ff {
-			c.PC += 4
-		} else {
+		x := (op & 0x0F00) >> 8
+		c.PC += 2
+		if c.V[x] == byte(op) {
 			c.PC += 2
 		}
 	case 0x4000:
-		if uint16(c.V[op&0x0f00>>8]) != op&0x00ff {
-			c.PC += 4
-		} else {
+		x := (op & 0x0F00) >> 8
+		c.PC += 2
+		if c.V[x] != byte(op) {
 			c.PC += 2
 		}
 	case 0x5000:
-		if c.V[op&0x0f00>>8] == c.V[op&0x00f0>>4] {
-			c.PC += 4
-		} else {
+		x := (op & 0x0F00) >> 8
+		y := (op & 0x00F0) >> 4
+		c.PC += 2
+		if c.V[x] == c.V[y] {
 			c.PC += 2
 		}
 	case 0x6000:
-		c.V[op&0x0f00>>8] = uint8(op & 0x00ff)
+		x := (op & 0x0F00) >> 8
+		kk := byte(op)
+		c.V[x] = kk
 		c.PC += 2
 	case 0x7000:
-		c.V[op&0x0f00>>8] += uint8(op & 0x00ff)
+		x := (op & 0x0F00) >> 8
+		kk := byte(op)
+		c.V[x] = c.V[x] + kk
 		c.PC += 2
 	case 0x8000:
+		x := (op & 0x0F00) >> 8
+		y := (op & 0x00F0) >> 4
 		switch op & 0x000f {
 		case 0x0000:
-			c.V[op&0x0f00>>8] = c.V[op&0x00f0>>4]
+			c.V[x] = c.V[y]
 			c.PC += 2
 		case 0x0001:
-			c.V[op&0x0f00>>8] |= c.V[op&0x00f0>>4]
+			c.V[x] |= c.V[y]
 			c.PC += 2
 		case 0x0002:
-			c.V[op&0x0f00>>8] &= c.V[op&0x00f0>>4]
+			c.V[x] &= c.V[y]
 			c.PC += 2
 		case 0x0003:
-			c.V[op&0x0f00>>8] ^= c.V[op&0x00f0>>4]
+			c.V[x] ^= c.V[y]
 			c.PC += 2
 		case 0x0004:
-			if c.V[op&0x00f0>>4] > (0xff - c.V[op&0x0f00>>8]) {
-				c.V[0xf] = 1
-			} else {
-				c.V[0xf] = 0
+			r := uint16(c.V[x]) + uint16(c.V[y])
+			var cf byte
+			if r > 0xFF {
+				cf = 1
 			}
-			c.V[op&0x0f00>>8] += c.V[op&0x00f0>>4]
+			c.V[0xF] = cf
+			c.V[x] = byte(r)
 			c.PC += 2
 		case 0x0005:
-			if c.V[op&0x00f0>>4] > c.V[op&0x0f00>>8] {
-				c.V[0xf] = 0
-			} else {
-				c.V[0xf] = 1
+			var cf byte
+			if c.V[x] > c.V[y] {
+				cf = 1
 			}
-			c.V[op&0x0f00>>8] -= c.V[op&0x00f0>>4]
+			c.V[0xF] = cf
+			c.V[x] = c.V[x] - c.V[y]
 			c.PC += 2
 		case 0x0006:
-			c.V[0xf] = c.V[op&0x0f00>>8] & 0x1
-			c.V[op&0x0f00>>8] >>= 1
+			var cf byte
+			if (c.V[x] & 0x01) == 0x01 {
+				cf = 1
+			}
+			c.V[0xF] = cf
+			c.V[x] = c.V[x] / 2
 			c.PC += 2
 		case 0x0007:
-			if c.V[op&0x00f0>>4] < c.V[op&0x0f00>>8] {
-				c.V[0xf] = 0
-			} else {
-				c.V[0xf] = 1
+			var cf byte
+			if c.V[y] > c.V[x] {
+				cf = 1
 			}
-			c.V[op&0x0f00>>8] = c.V[op&0x00f0>>4] - c.V[op&0x0f00>>8]
+			c.V[0xF] = cf
+			c.V[x] = c.V[y] - c.V[x]
 			c.PC += 2
 		case 0x000e:
-			c.V[0xf] = c.V[op&0x0f00>>8] >> 7 //& 0x80
-			c.V[op&0x0f00>>8] <<= 1
+			var cf byte
+			if (c.V[x] & 0x80) == 0x80 {
+				cf = 1
+			}
+			c.V[0xF] = cf
+			c.V[x] = c.V[x] * 2
 			c.PC += 2
+		default:
+			panic("Not implemented")
 		}
 	case 0x9000:
-		if c.V[op&0x0f00>>8] != c.V[op&0x00f0>>4] {
-			c.PC += 4
-		} else {
+		x := (op & 0x0F00) >> 8
+		y := (op & 0x00F0) >> 4
+		c.PC += 2
+		if c.V[x] != c.V[y] {
 			c.PC += 2
 		}
 	case 0xa000:
-		c.I = op & 0x0fff
+		c.I = op & 0x0FFF
 		c.PC += 2
 	case 0xb000:
-		c.PC = op&0x0fff + uint16(c.V[0])
+		c.PC = op&0x0FFF + uint16(c.V[0])
 	case 0xc000:
-		c.V[op&0x0f00>>8] = uint8(uint16(rand.Int()%0xff) & (op & 0x00ff))
+		x := (op & 0x0F00) >> 8
+		kk := byte(op)
+		c.V[x] = kk + uint8(uint16(rand.Int()%0xff))
 		c.PC += 2
 	case 0xd000:
 		height := uint8(op & 0x000f)
@@ -127,63 +149,64 @@ func (c *Chip8) emulateCycle() {
 				}
 			}
 		}
-
 		c.Draw = true
 		c.PC += 2
 	case 0xe000:
+		x := (op & 0x0F00) >> 8
 		switch op & 0x00ff {
 		case 0x009e:
-			if c.V[op&0x0f00>>8] == c.Key {
-				c.PC += 4
-			} else {
+			c.PC += 2
+			if c.V[x] == c.Key {
 				c.PC += 2
 			}
 		case 0x00a1:
-			if c.Key == c.V[op&0x0f00>>8] {
+			c.PC += 2
+			if c.V[x] != c.Key {
 				c.PC += 2
-			} else {
-				c.PC += 4
 			}
+		default:
+			panic("Not implemented")
 		}
 		c.Key = 0
 	case 0xf000:
+		x := (op & 0x0F00) >> 8
 		switch op & 0x00ff {
 		case 0x0007:
-			c.V[op&0x0f00>>8] = c.DT
+			c.V[x] = c.DT
 			c.PC += 2
 		case 0x000a:
-			c.V[op&0x0f00>>8] = c.Key // value of keyboard
+			c.V[x] = c.Key
 			c.Key = 0
 			c.PC += 2
 		case 0x0015:
-			c.DT = c.V[op&0x0f00>>8]
+			c.DT = c.V[x]
 			c.PC += 2
 		case 0x0018:
-			c.ST = c.V[op&0x0f00>>8]
+			c.ST = c.V[x]
 			c.PC += 2
 		case 0x001e:
-			c.I += uint16(c.V[(op&0x0F00)>>8])
+			c.I = c.I + uint16(c.V[x])
 			c.PC += 2
 		case 0x0029:
-			c.I = 0x05 * (op & 0x0f00 >> 8)
+			c.I = uint16(c.V[x]) * uint16(0x05)
 			c.PC += 2
 		case 0x0033:
-			c.Mem[c.I] = c.V[op&0x0f00>>8] / 100
-			c.Mem[c.I+1] = (c.V[op&0x0f00>>8] / 10) % 10
-			c.Mem[c.I+2] = (c.V[op&0x0f00>>8] % 100) % 10
+			c.Mem[c.I] = c.V[x] / 100
+			c.Mem[c.I+1] = (c.V[x] / 10) % 10
+			c.Mem[c.I+2] = (c.V[x] % 100) % 10
 			c.PC += 2
 		case 0x0055:
-			for i := uint16(0); i <= (op & 0x0f00 >> 8); i++ {
-				c.Mem[i+i] = c.V[i]
+			for i := 0; uint16(i) <= x; i++ {
+				c.Mem[c.I+uint16(i)] = c.V[i]
 			}
-			c.I += (op & 0x0f00 >> 8) + 1
 			c.PC += 2
 		case 0x0065:
-			for i := uint16(0); i <= (op & 0x0f00 >> 8); i++ {
-				c.V[i] = c.Mem[c.I+i]
+			for i := 0; byte(i) <= byte(x); i++ {
+				c.V[uint16(i)] = c.Mem[c.I+uint16(i)]
 			}
-			c.I += (op & 0x0f00 >> 8) + 1
 			c.PC += 2
+		default:
+			panic("Not implemented")
 		}
 	default:
 		panic("Not implemented")
